a0446f9998d200c7cda537a82f28f068300137de
park910517 <77248984+park910517@users.noreply.github.com>
2022-01-24 16:28:14 +0900

    로스트아크 캐릭터관리 0.4.0버전

---
+++ LostArkCharacterV0.4.0/src/com/park/lostark/data/CharacterList.java
@@ -0,0 +1,47 @@
+package com.park.lostark.data;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.park.lostark.display.Title;
+
+public class CharacterList {
+
+	String back = "back";
+
+	public void run(ArrayList<Character> characters) {
+		System.out.print("명령어 입력(1.전체목록 2.검색 (뒤로가기는 back입력)):");
+		Scanner sc = new Scanner(System.in);
+		String cmd = sc.next();
+		switch (cmd) {
+		case "1":
+			System.out.println(Title.LIST);
+			for (Character c : characters) {
+				c.info();
+			}
+			break;
+		case "2":
+			System.out.println(Title.SEARCH);
+			System.out.print("검색하고 싶은 캐릭터 닉네임:");
+			String characterSearch = sc.next();
+			for (int i = 0; i < characters.size(); i++) {
+				if (characters.get(i).getName().equals(characterSearch)) {
+					characters.get(i).info();
+					return;
+				}
+			}
+
+			if (back.equals(characterSearch)) {
+				return;
+
+			}
+			boolean search = characters.contains(characterSearch);
+			if (search == false) {
+				System.out.println("그런 캐릭터는 없습니다. 닉네임을 확인해주세요.");
+				return;
+			}
+
+		}
+
+	}
+}
